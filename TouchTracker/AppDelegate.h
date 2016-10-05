ba32513f3d05d847e22d4920ecd8074343e5e71c
CloudnuY <cloudnuy@gmail.com>
2016-10-05 17:25:26 +0800

    Init

---
+++ TouchTracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TouchTracker
+//
+//  Created by Cloud on 2016/10/5.
+//  Copyright © 2016年 Cloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
